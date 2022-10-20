a9d56adb2dabbd8e06e778969a6516f2c3e229fc
ahmed <dwainir@gmail.com>
2022-10-20 10:10:38 +0100

    first commit - init project

---
+++ src/test/java/ma/liquibase/demo/DemoLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.liquibase.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
